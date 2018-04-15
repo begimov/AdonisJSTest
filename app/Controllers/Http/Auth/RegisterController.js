08fdc7503c4d61f383fe94f098f2f1822d35ea04
begimov <begimov@gmail.com>
2018-04-15 09:47:07 +0300

    Validator used to validate incoming register data

--- app/Controllers/Http/Auth/RegisterController.js
+++ app/Controllers/Http/Auth/RegisterController.js
@@ -1,11 +1,13 @@
 'use strict'
 
+const { validateAll } = use('Validator')
+
 class RegisterController {
     index ({ view }) {
         return view.render('auth.register')
     }
 
-    register ({ request }) {
+    async register ({ request }) {
         const { email, username, password } = request.all()
 
         const rules = {
@@ -13,6 +15,10 @@ class RegisterController {
             username: 'required|unique:users,username',
             password: 'required'
         }
+
+        const validation = await validateAll(request.all(), rules)
+
+        return validation.fails()
     }
 }
 
