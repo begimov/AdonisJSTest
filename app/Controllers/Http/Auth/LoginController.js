cb7d5f87a599d73b4358437122e8cc5482a23411
begimov <begimov@gmail.com>
2018-04-15 12:13:33 +0300

    validation for a login added

--- app/Controllers/Http/Auth/LoginController.js
+++ app/Controllers/Http/Auth/LoginController.js
@@ -1,20 +1,29 @@
 'use strict'
 
-class LoginController {
+const { validateAll } = use('Validator')
 
+class LoginController {
     index ({ view }) {
         return view.render('auth.login')
     }
 
-    login({ request, response, session }) {
+    async login({ request, response, session }) {
         const { email, password } = request.all()
 
         const rules = {
             email: 'required|email',
             password: 'required'
         }
-    }
 
+        const validation = await validateAll(request.all(), rules)
+
+        if(validation.fails()) {
+            session.withErrors(validation.messages()).flashAll()
+            return response.redirect('back')
+        }
+
+        return '!!!!'
+    }
 }
 
 module.exports = LoginController
