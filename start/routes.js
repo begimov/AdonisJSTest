da7eee1b24b98f3ce887b775650f1cbf7384c2c9
begimov <begimov@gmail.com>
2018-04-15 11:57:55 +0300

    login related routes created

--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,6 @@ Route.on('/').render('welcome').as('home')
 
 Route.get('/auth/register', 'Auth/RegisterController.index').as('auth.register')
 Route.post('/auth/register', 'Auth/RegisterController.register').as('auth.register')
+
+Route.get('/auth/login', 'Auth/LoginController.index').as('auth.login')
+Route.post('/auth/login', 'Auth/LoginController.login').as('auth.login')
